84d97b217e52deb9a86a23991f717adda1665665
Dornavan <130479724+dorndickence@users.noreply.github.com>
2023-10-30 07:35:51 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-TOKEN = ""      # Bot Father Token
-USERNAME = ""   # YOUR-EMAIL
-PASSWORD = ""   # YOUR-PASSWORD
+TOKEN = " 6692506798:AAFk6xRWnlex19mup4fKhbWkpxfJvEdimh0"      # Bot Father Token
+USERNAME = "dorndickence@gmail.com"   # YOUR-EMAIL
+PASSWORD = "710770837dorn?"   # YOUR-PASSWORD
